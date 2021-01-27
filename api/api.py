008d9286f95973c05f518a084126b5308146607f
janus <janus@insignificancegalore.net>
2021-01-27 09:30:28 +0100

    Move authentication ID's to .env.local
    This solution is compatible with env vars for deployment.

--- api/api.py
+++ api/api.py
@@ -1,13 +1,19 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-
 from flask_azure_oauth import FlaskAzureOauth
+import dotenv
+import os
+from pathlib import Path
+
 
 app = Flask(__name__)
 CORS(app)
 
-app.config["AZURE_OAUTH_APPLICATION_ID"] = "{Application (client) ID for backend app registration}"
-app.config["AZURE_OAUTH_TENANCY"] = "{Directory (tenant) ID}"
+
+dotenv.load_dotenv(Path(__file__).parent / '../app/.env.local')
+app.config["AZURE_OAUTH_APPLICATION_ID"] = os.environ["VUE_APP_AZURE_OAUTH_BACKEND_APPLICATION_ID"]
+app.config["AZURE_OAUTH_TENANCY"] = os.environ["VUE_APP_AZURE_OAUTH_TENANCY"]
+
 
 auth = FlaskAzureOauth()
 auth.init_app(app)
